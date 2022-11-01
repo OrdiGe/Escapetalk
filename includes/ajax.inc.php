cb96af7a1e67689d13ce934e00b1c1357a9f3adb
Geordi Bonnie <gjad.bonnie@gmail.com>
2022-11-01 16:01:56 +0100

    commit

--- includes/ajax.inc.php
+++ includes/ajax.inc.php
@@ -1,29 +1,54 @@
 <?php
 
-if($_POST['type'] == 'claimBadge')
+if($_POST['type'] == 'claimBadges')
 {
     $badges = [
         1 => ["id" => 1, "title" => "profileCompleteBadge", "progress" => "1/1", "claimable" => true, "icon" => "profileCompleteBadge.svg"],
-        2 => ["id" => 2, "title" => "100likesBadge", "progress" => "0/100", "claimable" => false, "icon" => "100likesBadge.svg"],
-        3 => ["id" => 3, "title" => "championBadge", "progress" => "0/1", "claimable" => false, "icon" => "championBadge.svg"],
-        4 => ["id" => 4, "title" => "eersteKamerReviewBadge", "progress" => "0/1", "claimable" => false, "icon" => "eersteKamerReview.svg"],
-        5 => ["id" => 5, "title" => "firstGamesReviewBadge", "progress" => "0/1", "claimable" => false, "icon" => "firstGamesReview.svg"],
-        6 => ["id" => 6, "title" => "foundersBadge", "progress" => "0/1", "claimable" => false, "icon" => "foundersBadge.svg"],
-        7 => ["id" => 7, "title" => "halloweenBadge", "progress" => "0/1", "claimable" => false, "icon" => "halloweenBadge.svg"],
-        8 => ["id" => 8, "title" => "1jaarLidBadge", "progress" => "0/1", "claimable" => false, "icon" => "1JaarLidBadge.svg"],
+        2 => ["id" => 2, "title" => "firstRoomPlayedBadge", "progress" => "1/1", "claimable" => true, "icon" => "firstRoomPlayedBadge.svg"],
+        3 => ["id" => 3, "title" => "firstGamePlayedBadge", "progress" => "0/1", "claimable" => true, "icon" => "firstGamePlayedBadge.svg"],
+        4 => ["id" => 4, "title" => "firstRoomReviewBadge", "progress" => "1/1", "claimable" => true, "icon" => "firstRoomReview.svg"],
+        5 => ["id" => 5, "title" => "firstGameReviewBadge", "progress" => "0/1", "claimable" => true, "icon" => "firstGameReview.svg"],
+        6 => ["id" => 6, "title" => "10RoomsOneMonth", "progress" => "0/1", "claimable" => false, "icon" => "10RoomsOneMonth.svg"],
+        7 => ["id" => 7, "title" => "1yearMemberBadge", "progress" => "0/1", "claimable" => true, "icon" => "1yearMemberBadge.svg"],
+        8 => ["id" => 8, "title" => "halloweenBadge", "progress" => "0/1", "claimable" => true, "icon" => "halloweenBadge.svg"],
+        
     ];
 
-    $badge_id = $_POST["badgeId"];
+    $return = [];
 
-    if($badge_id && $badges[$badge_id]["claimable"] == true)
+    $badges_post = json_decode($_POST['badges']);
+
+
+
+    foreach($badges_post as $badge)
     {
-        echo json_encode($badges[$badge_id]);
+        if(isset($badges[$badge]) && $badges[$badge]['claimable'] == true)
+        {
+            $return[] = $badges[$badge];
+        }
     }
-    else 
-    {
-        echo '0';
-    }
+
+    echo json_encode($return);
+
+    die();
 }
 
-die();
 ?>
+
+
+<!-- // 1 claim 
+
+$items = [$id];
+
+
+//claimall 
+
+loop over alle beschikbare,  en zet die allemaal in array
+
+var items = [];
+
+foreach(claimable as id)
+{
+    items.push(id);
+
+} -->
